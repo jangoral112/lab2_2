773b692ce555adf375ea345204b77184771b03f5
Jan Góral <jan.goral112@gmail.com>
2021-03-22 17:57:37 +0100

    Added test shouldReturnOneWhenBothSequenceAreEmpty in SimilarityFinderTest

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,10 +1,29 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
+    @Test
+    public void shouldReturnOneWhenBothSequenceAreEmpty() {
+        // given
+
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> null);
+
+        int[] seq1 = {};
+        int[] seq2 = {};
+        double expectedValue = 1;
+
+        // when
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        // then
+        assertEquals(expectedValue, result);
+    }
 
 }
